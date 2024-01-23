601c75319699c3452d88498d73acb27cde70302c
Alexey Inkov <alexo982@mail.ru>
2024-01-24 02:33:28 +0300

    add forms

---
+++ mysite/shoplist/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from .models import ShoppingListItem, Product
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = 'name', 'price'
+
+class ShoppingListForm(ModelForm):
+    class Meta:
+        model = ShoppingListItem
+        fields = 'product', 'count'
