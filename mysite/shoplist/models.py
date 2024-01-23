3ff1dfb40d258257e6420defadad7e03ff4ee10d
Alexey Inkov <alexo982@mail.ru>
2024-01-23 21:49:42 +0300

    create models

--- mysite/shoplist/models.py
+++ mysite/shoplist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    class Meta:
+        ordering = ["name", "price"]
+
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Product(pk={self.pk}, name={self.name!r})"
+
+
+class ShoppingListItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField(default=1)
