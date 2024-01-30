27cf0e5ef14b6fa42259eb0322d49c1c6d7a6d3e
Alexey Inkov <alexo982@mail.ru>
2024-01-30 23:27:25 +0300

    del file old bd

--- mysite/shoplist/forms.py
+++ mysite/shoplist/forms.py
@@ -1,11 +1,14 @@
 from django.forms import ModelForm
 from .models import ShoppingListItem, Product
+
+
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = 'name', 'price'
+        fields = "name", "price"
+
 
 class ShoppingListForm(ModelForm):
     class Meta:
         model = ShoppingListItem
-        fields = 'product', 'count'
+        fields = "product", "count"
